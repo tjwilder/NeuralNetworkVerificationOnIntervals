23d1521ac53a068397b06c933928d7cfdaf4f666
tj <tj@wilder.tj>
2020-04-10 17:47:11 -0500

    Switched to tensor operations

--- README.md
+++ README.md
@@ -10,22 +10,66 @@ To install Pytorch
 pip3 install torch torchvision
 ```
 
-To run the program
+To train the network
+
+```
+python3 feed-forward-nn.py train
+```
+
+To run the program without training
+
 ```
 python3 feed-forward-nn.py
 ```
 
-## Goals
+## Optimization ##
 
-This assignment is designed to help you understand abstraction-based techniques for verifying neural networks. Specifically, we will implement the simplest abstract domain discussed in class, the intervals abstraction.
+- I originally wrote everything using a Range class and operations over that
+    - Each NN forward pass took 0.24s (40 minutes total per epsilon)
+- I tried changing to tuples instead of a class
+    - Each NN forward pass took 0.22s (37 minutes total per epsilon)
+- I changed to using tensor operations over lists of lower  and upper bounds
+    - Though this require more total operations, they were internally optimized
+    - Each NN forward pass took 0.013s (2.2 minutes total per epsilon)
 
-## The code
+## Output ##
 
-The code supplied is *exactly* the same one used in A1.
+This is the output for the "test" version. You can re-train the model by using `python feed-forward-nn.py train`. Epsilon is the amount that each pixel can change up and down (ie `range = (x_i - epsilon, x_i + epsilon)` for all `i`)
+The "robust amount" means that the lower bound of the correct label's interval is greater than the upper bounds of all incorrect labels. This means that the maximum robustness is the normal accuracy (92.54%)
 
-## Original Output ##
-
-8989 / 10000 for e=.001 across the whole test set but took like an hour
+```
+Timothys-MBP:A2-interval-abstraction tj$ python feed-forward-nn.py
+Average time (1000 images): 0.013858854055404664
+Average time (1000 images): 0.013556383848190308
+Average time (1000 images): 0.014038251876831056
+Average time (1000 images): 0.013524255990982056
+Average time (1000 images): 0.01340032696723938
+Average time (1000 images): 0.013654764890670777
+Average time (1000 images): 0.013518330097198486
+Average time (1000 images): 0.013457827091217041
+Average time (1000 images): 0.013374399900436401
+Robust amount for epsilon=0.001: 8989/10000
+Average time (1000 images): 0.01341677713394165
+Average time (1000 images): 0.013441201210021972
+Average time (1000 images): 0.01343557596206665
+Average time (1000 images): 0.013371975898742675
+Average time (1000 images): 0.013478943824768067
+Average time (1000 images): 0.01340594482421875
+Average time (1000 images): 0.013447988033294677
+Average time (1000 images): 0.01340087604522705
+Average time (1000 images): 0.013499964952468873
+Robust amount for epsilon=0.005: 6988/10000
+Average time (1000 images): 0.013482644319534302
+Average time (1000 images): 0.01337493896484375
+Average time (1000 images): 0.013404582738876342
+Average time (1000 images): 0.01344388508796692
+Average time (1000 images): 0.01343005895614624
+Average time (1000 images): 0.013392107009887695
+Average time (1000 images): 0.013400310039520264
+Average time (1000 images): 0.01338854193687439
+Average time (1000 images): 0.013386988878250123
+Robust amount for epsilon=0.01: 2849/10000
+```
 
 ## Your job
 
@@ -45,7 +89,7 @@ Consult the notes for more details.
 
 Here's how I would approach this assignment:
 1. Create a data structure that represents a vector of intervals.
-2. Implement all necessary operations over that data structure to mimic those performed by the neural network. For example, our network contains a ReLU layer (```self.relu(x)```), so you need a version of that function that applies ReLUs over intervals (```self.relu_interval(xi)```), etc.
+2. Implement all necessary operations over that data structure to mimic those performed by the neural network. For example, our network contains a ReLU layer (`self.relu(x)`), so you need a version of that function that applies ReLUs over intervals (`self.relu_interval(xi)`), etc.
 
 What to hand in:
 1. Unlike with A1, this should be really fast. So pick 3 values of epsilon, and run the verification on the whole test set of images, and submit the code that iterates through the dataset. You might not be able to prove a lot of images robust (remember, with abstraction-based techniques, sometimes the answer is *Don't know* when the abstraction is too imprecise). If you find that's the case, make epsilon smaller, or make it non-zero only for the first N pixels instead of all pixels.
--- feed-forward-nn.py
+++ feed-forward-nn.py
@@ -1,4 +1,3 @@
-import functools
 import time
 import sys
 import torch
@@ -44,69 +43,56 @@ class NeuralNet(nn.Module):
     return out
 
 
-# I originally made a Range class, but  it was slow so I changed it to use
-#  tuples. However this only changed the average time .24 => .22
-def r_add(this, other):
-  lower = this[0] + other[0]
-  upper = this[1] + other[1]
-  return (lower, upper)
+def ranged_dot(lower, upper, w, b):
+  """
+  Calculate the dot product of (w * x) where each
+    input x_i is a range (lower_i, upper_i)
 
+  Keyword arguments:
+  lower -- a list of lower bounds of x
+  upper -- a list of upper bounds of x
+  w -- the 2d matrix to multiply by
+  b -- bias term
+  """
+  lowers = torch.tensor(lower).repeat(len(w), 1)
+  uppers = torch.tensor(upper).repeat(len(w), 1)
+  # Element-wise product of each (x_l, x_u) with the weights
+  y_lowers = w * lowers
+  y_uppers = w * uppers
 
-def r_add_scalar(this, scalar):
-  lower = this[0] + scalar
-  upper = this[1] + scalar
-  return (lower, upper)
+  # Since a negative weight will swap lower/upper bounds:
+  # 1. Take the element-wise minimum and maximum
+  # 2. Sum along the output dimension
+  # 3. Add the bias
+  y_lower = torch.min(y_lowers, y_uppers)
+  y_lower = y_lower.sum(1) + b
+  y_upper = torch.max(y_lowers, y_uppers)
+  y_upper = y_upper.sum(1) + b
 
-
-def r_scale(this, factor):
-  min_num = this[0] * factor
-  max_num = this[1] * factor
-  # Account for negative factors
-  lower = min(min_num, max_num)
-  upper = max(min_num, max_num)
-  return (lower, upper)
-
-
-def r_relu(this):
-  if this[1] < 0:
-    return (0, 0)
-  if this[0] > 0:
-    return (this[0], this[1])
-
-  return (0, this[1])
-
-
-def ranged_dot(ranges, w, b):
-  # Scale all ranges by appropriate weights
-  y = [[r_scale(ranges[i], w[j][i]) for i in range(len(ranges))]
-       for j in range(len(w))]
-  # Sum them together to complete the dot product
-  y = [functools.reduce(lambda a, b: r_add(a, b), y[j])
-       for j in range(len(y))]
-  # Add bias
-  y = [r_add_scalar(y[i], b[i]) for i in range(len(y))]
-
-  return y
+  return y_lower, y_upper
 
 
 def forward_range(epsilon, x):
-  ranged_x = [(p - epsilon, p + epsilon) for p in x]
+  lower = [p - epsilon for p in x]
+  upper = [p + epsilon for p in x]
   # applies linear transformation to the input data, y = wx + b
   w = fetch_weights(1)
   b = fetch_bias(1)
-  y_1 = ranged_dot(ranged_x, w, b)
+  lower, upper = ranged_dot(lower, upper, w, b)
 
   # applies the RELU activation function on the hidden layer
-  y_2 = [r_relu(y) for y in y_1]
+  lower = [model.relu(x) for x in lower]
+  upper = [model.relu(x) for x in upper]
 
   # applies linear transformation to the hidden layer to map to the
   # output layer
   w = fetch_weights(2)
   b = fetch_bias(2)
   # Scale all ranges by appropriate weights
-  y_3 = ranged_dot(y_2, w, b)
+  lower, upper = ranged_dot(lower, upper, w, b)
 
-  return y_3
+  # Put each output into a tuple of (lower, upper)
+  return [(lower[i], upper[i]) for i in range(len(lower))]
 
 
 def range_matches(ranges, label):
@@ -239,11 +225,9 @@ def fetch_weights(layer):
 
 
 def fetch_bias(layer):
-  # Fetch bias term
+  # Fetch bias term from the correct layer
   if layer == 1:
     return model.fc1.bias
-  # Weights between input layer and hidden layer (Tensor of shape: [10,
-  # #hidden] i.e. output layer size * hidden layer size)
   if layer == 2:
     return model.fc2.bias
 
@@ -255,22 +239,21 @@ def plot_dataset(image):
 
 def test_ranged(epsilon):
   robust = 0
+  over = 1000
   t = time.time()
   for i in range(len(test_dataset)):
     image = test_dataset[i][0]
     image = image.reshape(-1, 28 * 28).to(device)[0]
     label = test_dataset[i][1]
     ranges = forward_range(epsilon, image)
-    # print(ranges)
+
     if range_matches(ranges, label):
       robust += 1
-      # print(True)
-    # else:
-      # print(False)
-    # print(model.forward(image))
-    if i % 100 == 0 and i != 0:
-      print('Average time (over 100 samples):', (time.time() - t) / 100)
+
+    if i % over == 0 and i != 0:
+      print(f'Average time ({over} images): {(time.time() - t) / over}')
       t = time.time()
+
   print(f'Robust amount for epsilon={epsilon}: {robust}/{len(test_dataset)}')
 
 
@@ -287,14 +270,9 @@ def main():
   else:
     load_model()
   # test()
-  epsilons = [0.001, 0.002, 0.003]
+  epsilons = [0.001, 0.005, 0.010]
   for epsilon in epsilons:
     test_ranged(epsilon)
-
-  # Print first image in the test dataset
-  # for i, image in enumerate(test_dataset):
-  #   if i == 0:
-  #     plot_dataset(image)
 
 
 if __name__ == '__main__':
